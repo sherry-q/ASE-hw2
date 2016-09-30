6bbab951ab3ecc675643e581516976daafa06e4d
Sherry Qiu <sherryq@dyn-160-39-131-195.dyn.columbia.edu>
2016-09-30 16:43:06 -0400

    base for web app

---
+++ app.py
@@ -0,0 +1,62 @@
+from flask import Flask, request, flash, url_for, redirect, render_template
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///students.sqlite3'
+app.config['SECRET_KEY'] = "random string"
+
+db = SQLAlchemy(app)
+
+class students(db.Model):
+   id = db.Column('student_id', db.Integer, primary_key = True)
+   name = db.Column(db.String(100))
+   city = db.Column(db.String(50))
+   addr = db.Column(db.String(200)) 
+   pin = db.Column(db.String(10))
+
+   def __init__(self, name, city, addr,pin):
+      self.name = name
+      self.city = city
+      self.addr = addr
+      self.pin = pin
+
+@app.route('/')
+def show_all():
+   print students.query.all()
+   return render_template('show_all.html', students = students.query.all() )
+
+@app.route('/new', methods = ['GET', 'POST'])
+def new():
+   if request.method == 'POST':
+      if not request.form['name'] or not request.form['city'] or not request.form['addr'] or not request.form['pin']:
+         flash('Please enter all the fields', 'error')
+      else:
+         student = students(request.form['name'], request.form['city'], request.form['addr'], request.form['pin'])  
+         #if request.form['add']
+         db.session.add(student)
+         db.session.commit()
+         flash('Record was successfully added')
+         #else:
+         #   db.session.delete(student)
+         #   db.session.commit()
+         #   flash('Record was successfully removed')
+         return redirect(url_for('show_all'))
+   return render_template('new.html')
+"""
+@app.route('/remove', methods = ['GET', 'POST'])
+def remove():
+   if request.method == 'POST':
+      if not request.form['name'] or not request.form['city'] or not request.form['addr'] or not request.form['pin']:
+         flash('Please enter all the fields', 'error')
+      else:
+         student = students(request.form['name'], request.form['city'], request.form['addr'], request.form['pin'])
+         db.session.delete(student)
+         db.session.commit()
+         flash('Record was successfully removed')
+         return redirect(url_for('show_all'))
+   return render_template('new.html')
+"""
+
+if __name__ == '__main__':
+   db.create_all()
+   app.run(debug = True)
