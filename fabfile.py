decd9a1444ba52d4ec8f3744b47a45a70f9dbe4f
Sherry Qiu <sherryq6@gmail.com>
2016-10-04 13:25:10 -0400

    "add static analysis results"

--- fabfile.py
+++ fabfile.py
@@ -13,13 +13,15 @@ def test():
 		abort("Aborting at user request.")
 
 def static_analysis():
+	# ignore error from static analysis so files can be pushed to git
 	try:
 		local("pylint shopping_list.py >> shopping_list_static_analysis.txt")
 	except:
 		pass
 
 def commit():
-	local("git add -p && git commit")
+	local("git add -u && git commit")
+	local("git add shopping_list_static_analysis.txt")
 
 def push():
 	local("git push")
