2f21f2489d928faa7f4652ec186c9319bff5eb76
Sherry Qiu <sherryq6@gmail.com>
2016-10-04 00:43:19 -0400

    add deploy to fabfile

--- fabfile.py
+++ fabfile.py
@@ -1,9 +1,13 @@
+# reference tutorial: http://docs.fabfile.org/en/1.12/tutorial.html
+
 from __future__ import with_statement
 from fabric.api import *
 from fabric.contrib.console import confirm
+import os
 
-env.hosts = ['my_server']
+#env.hosts = ['shopping_list_server']
 
+# run unit tests
 def test():
 	with settings(warn_only=True):
 		result = local('python test.py', capture=True)
@@ -20,3 +24,13 @@ def prepare_deploy():
 	test()
 	commit()
 	push()
+
+# start server for web app
+def deploy():
+	# we don't need this right? because it's for remove servers?
+	#with settings(warn_only=True):
+    #    if run("test -d %s" % code_dir).failed:
+    #        run("git clone user@vcshost:/path/to/repo/.git %s" % code_dir)
+    with cd(os.getcwd()):
+        local("git pull")
+        local("python shopping_list.py")
